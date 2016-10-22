dceb2276a2028f376b2dc2a08be468a1f3a92f31
Maxim Ryndin <masim_r@mail.ru>
2016-10-22 10:01:45 +0300

    Rename containers.

---
+++ src/containers/CloseDoneTodoButton.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class CloseDoneTodoButton extends Component {
+  render() {
+    return (
+      <div className="CloseDoneTodoButton">
+      </div>
+    );
+  }
+}
+
+export default CloseDoneTodoButton
---
+++ src/containers/CreateNewTodoButton.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class CreateNewTodoButton extends Component {
+  render() {
+    return (
+      <div className="CreateNewTodoButton">
+      </div>
+    );
+  }
+}
+
+export default CreateNewTodoButton
